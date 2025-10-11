8b05fc2fd8a7f3ebac8abf93a3ca4075832cdccd
v-ishwa <m.viswa001@gmail.com>
2025-10-11 17:23:01 +0530

    feat: Authentication before testing

--- Backend/src/controllers/authControllers.js
+++ Backend/src/controllers/authControllers.js
@@ -1,9 +1,12 @@
-import pool from "../config/db.js";
-import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import pool from "../config/db.js";
+import { generateToken } from "../utils/generateToken.js";
 
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
+
+  if (!name || !email || !password)
+    return res.status(400).json({ message: "All fields are required" });
 
   try {
     const existingUser = await pool.query(
@@ -16,14 +19,17 @@ export const registerUser = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await pool.query(
+    const result = await pool.query(
       "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email",
       [name, email, hashedPassword]
     );
 
+    const newUser = result.rows[0];
+    const token = generateToken(newUser.id);
+
     return res
       .status(201)
-      .json({ message: "User registered successfully", user: newUser.rows[0] });
+      .json({ message: "User registered successfully", user: newUser });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
@@ -32,18 +38,31 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
   const { email, password } = req.body();
 
+  if (!email || !password)
+    return res.status(400).json({ message: "Email and password are required" });
+
   try {
-    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
       email,
     ]);
 
-    if (user.rows.length === 0)
-      return res.status(400).json({ message: "Invalid credentials" });
+    const user = result.rows[0];
+    
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
-    const isMatch = await bcrypt.compare(password, user.rows[0].password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
-    
-  } catch (error) {}
+
+    const token = generateToken(user.id);
+    res
+      .status(200)
+      .json({
+        message: "Login successful",
+        user: { id: user.id, name: user.name, email: user.email },
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
